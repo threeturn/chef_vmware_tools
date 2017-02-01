79114c0ca8f4082deec1676535d67ca27e723cc4
Alex Markessinis <markea@sage.edu>
2017-02-01 12:03:48 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# vmware_tools
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: vmware_tools
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
