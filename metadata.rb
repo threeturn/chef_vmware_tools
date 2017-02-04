0420ee253dc504c6d5ab846f5d3d1e9b9208d68b
Alex Markessinis <markea@sage.edu>
2017-02-04 14:21:41 -0500

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'vmware_tools'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Alex Markessinis'
+maintainer_email 'markea125@gmail.com'
 license 'MIT'
 description 'Installs vmware tools on virtual machines'
 long_description 'Installs vmware tools on virtual machines'
-version '0.1.3'
+version '0.1.4s'
 #supports 'windows'
 supports 'debian'
 supports 'ubuntu'
@@ -12,7 +12,7 @@ supports 'redhat'
 supports 'centos'
 supports 'fedora'
 chef_version "~> 12"
-cookbook 'yum-epel', '~> 2.1.1'
+suggests 'yum-epel', '~> 2.1.1'
 #depends 'chocolatey', '~> 1.1.0'
 issues_url 'https://github.com/MelonSmasher/chef_vmware_tools/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/MelonSmasher/chef_vmware_tools' if respond_to?(:source_url)
