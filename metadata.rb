c1b80f9e83a43f0c25be07428085677a004c3460
Alex Markessinis <markea@sage.edu>
2017-04-12 11:36:38 -0400

    fixed supported chef version

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email 'markea125@gmail.com'
 license 'MIT'
 description 'Installs vmware tools on virtual machines'
 long_description 'Installs vmware tools on virtual machines'
-version '0.1.4'
+version '0.1.5'
 #supports 'windows'
 supports 'debian'
 supports 'ubuntu'
 supports 'redhat'
 supports 'centos'
 supports 'fedora'
-chef_version "~> 12"
+chef_version '>= 12.1' if respond_to?(:chef_version)
 suggests 'yum-epel', '~> 2.1.1'
 #depends 'chocolatey', '~> 1.1.0'
 issues_url 'https://github.com/MelonSmasher/chef_vmware_tools/issues' if respond_to?(:issues_url)
